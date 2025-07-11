f64df8c59d34c14dfc0b448d03f097b425d5a175
anil kumar <akthinq@gmailcom>
2025-07-11 17:21:20 +0530

    changed backend alb r53 record name

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -40,8 +40,8 @@ resource "aws_lb_listener" "backend_alb" {
 # create r53 record for dns alb url
 resource "aws_route53_record" "backend_alb" {
   zone_id = var.zone_id
-  name    = "*.backend.${var.zone_name}"
-  # name    = "*.backend-dev.${var.zone_name}"
+  # name    = "*.backend.${var.zone_name}"
+  name    = "*.backend-dev.${var.zone_name}"
   type    = "A"
 
   alias {
